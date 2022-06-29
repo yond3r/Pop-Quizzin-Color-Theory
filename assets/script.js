37ca4a21798c612e0bb8d54394622d54fa577df7
Hope Barrow <hope@MacBook-Pro.local>
2022-06-29 18:18:07 -0500

    added game file

---
+++ assets/script.js
@@ -0,0 +1,27 @@
+// global var 
+// time left var
+// current question 
+// array of questions
+// each quetsion as an object
+
+[
+    {
+        question: "What are the four compontents of color?", 
+        choiceone: "tones, tints, nuetrals, grays",
+        choicetwo: "nuetralization, popability, dullability, chromatics",
+        choicethree: "hue, saturation, value, temperature",
+        choicefour: "complementaries, naturals, neutrals, warmth"
+     }
+]
+
+
+
+
+
+// function that starts game, that function needs to start timer -- function startGame
+// increments the question -- current
+// calls render question
+
+// new function "render queston"
+//check current what question we're on
+//display question + 4 answers on the html -- grab html 
