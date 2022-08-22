a2fbee086558b35c91f4551edd7ed92426a2afc6
Hope Barrow <barr.writing@gmail.com>
2022-08-21 20:39:53 -0500

    debugging + css fixes

--- assets/game/game.js
+++ assets/game/game.js
@@ -1,20 +1,4 @@
-var Q = 0;
-var question =document.getElementById("question")
-var choiceOne = document.getElementById("btnOne");
-var choiceTwo = document.getElementById("btnTwo");
-var choiceThree = document.getElementById("btnThree");
-var choiceFour = document.getElementById("btnFour");
-var wins =localStorage.getItem("savedWins")||0;
-winSpan.textContent=wins;
-var losses=localStorage.getItem("savedLosses")||0;
-lossSpan.textContent=losses;
-var timeRemaining=10;
-var timer
-
- startBtn.addEventListener("click", startGame);
-
-
-var questionsAll= [
+var question = [
     {
         question: "What are the four compontents of color?", 
         choices: {
@@ -67,60 +51,3 @@ var questionsAll= [
     },
 ]
 
-function questionsAnswered (){
-    choiceOne.textContent = questionsAll[Q].choices["choiceOne"]
-    choiceTwo.textContent = questionsAll[Q].choices["choiceTwo"]
-    choiceThree.textContent = questionsAll[Q].choices["choiceThree"]
-    choiceFour.textContent = questionsAll[Q].choices["choiceFour"]
-    
-    question.textContent = questionsAll[Q].question[0]
-    question.textContent = questionsAll[Q].question[1]
-    question.textContent = questionsAll[Q].question[2]
-    question.textContent = questionsAll[Q].question[3]
-    question.textContent = questionsAll[Q].question[4]
-       render ;
-}
-
-//btnOne.addEventListener("click", function () {
-
-//btnTwo.addEventListener("click", function () {
-
-//btnThree.addEventListener("click", function () {
-
-//btnFour.addEventListener("click", function () {
-
-    
-  //});
-
-
-//moving on from one question to the next
-setTimeout(() => {
-    loadQuestion();
-  }, 1000);
-
-//Add else, if else for questions
-
-    function setTimer() {
-         secondsRemaining--;
-        timeEl.textContent= "seconds remaining!";
-
-        if (secondsRemaining === 0)
-        alert("I'm sorry, you've lost")
-        finishQuiz();
-        losses++;
-        lossSpan.textContent=losses;
-        //attempting local storage, lol
-        localStorage.setItem("saveLosses", losses);
-        clearInterval(timer);
-        }
-    }, 1000)
-
-
-
-
-
-// increments the question -- current
-// calls render question
-
-// new function "render queston"
-//check current what question we're on
--- assets/script.js
+++ assets/script.js
@@ -1,9 +1,83 @@
-// Open game in new game.html & start game
 
-startBtn {
-    startBtn.addEventListener("click", );
+// var totalQuestions = question.length;
+var question = document.getElementById("question");
+
+var choiceOne = document.getElementById("btnOne");
+var choiceTwo = document.getElementById("btnTwo");
+var choiceThree = document.getElementById("btnThree");
+var choiceFour = document.getElementById("btnFour");
+
+var wins = localStorage.getItem("savedWins") || 0;
+// winSpan.textContent=wins;
+
+var losses = localStorage.getItem("savedLosses") || 0;
+// lossSpan.textContent= losses;
+
+//starting with the timer, because I'd like to get that working first, per instructions
+function startTimer(duration, display) {
+    var timer = duration, minutes, seconds;
+    setInterval(() => {
+        minutes = parseInt(timer / 60, 10);
+        seconds = parseInt(timer % 60, 10);
+
+        minutes = minutes < 10 ? "0" + minutes : minutes;
+        seconds = seconds < 10 ? "0" + seconds : seconds;
+
+        display.textContent = minutes + ":" + seconds;
+
+        if (--timer < 0) {
+            timer = duration
+        }
+        if (seconds === 0) {
+            alert('The game is finished! Congrats!')
+            finishQuiz();
+        }
+    }, 1000)
 }
 
-//I struggle with this everytime, but how in the love of god do I move the cute boxes to the center??
-//why is my html "return to home" tab (the OG index.html) not returning to home??
-//why is my pseudo code ::before not doing anything, I have big dreams w/ these custom buttons
+//starts questions w/ timer
+startBtn.addEventListener("click", startGame);
+    startBtn.addEventListener("click", function () {
+        var fiveMinutes = 60 * 5,
+            display = document.querySelector("#time");
+        startTimer(fiveMinutes, display);
+})
+
+//starts the game
+function startGame() {
+    function loadQuestion(questionIndex) {
+        document.getElementById("question").disabled = true;
+        var Q = question[questionIndex];
+        question.textContent(questionIndex + 1) + '.' + Q.question;
+        choiceOne.textContent = Q.btnOne;
+        choiceTwo.textContent = Q.btnTwo;
+        choiceThree.textContent = Q.btnThree;
+        choiceFour.textContent = Q.btnFour;
+    }
+};
+
+function loadNextQuestion() {
+    var selectedChoice = document.querySelector("answers");
+    if (!userChoice) {
+        alert("Hello! Please Select Your Answer! (It seems you have forgotten, but that is okay.)");
+        return;
+    }
+    var answers = selectedChoice.value;
+    if (question[currentAnswer].answers === answers) {
+        score += 1;
+    }
+
+    selectedChoice.checked = false;
+    currentQ++;
+    loadQuestion(currentQ);
+}
+
+//moving on from one question to the next
+setTimeout(() => {
+    loadQuestion();
+}, 1000);
+
+
+//attempting local storage, lol
+localStorage.setItem("saveLosses", losses);
+
